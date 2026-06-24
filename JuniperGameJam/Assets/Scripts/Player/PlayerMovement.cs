e8622e077dc016f45c16fc94f779aefff60ab6e0
LiamKing22 <lking122@outlook.com>
2026-06-23 19:18:10 -0600

    gordi got moves

--- JuniperGameJam/Assets/Scripts/Player/PlayerMovement.cs
+++ JuniperGameJam/Assets/Scripts/Player/PlayerMovement.cs
@@ -78,8 +78,8 @@ public class PlayerMovement : BaseMovement
 
         //apply any character movement
         Move();
-        //ApplyTilt();
-        Rotate();
+        ApplyTilt();
+        //Rotate();
 
         GainHeight();
         ReduceHeight();
@@ -162,12 +162,12 @@ public class PlayerMovement : BaseMovement
                 rb.AddForce(counteractDirection * decelerationRate, ForceMode.Acceleration);
             }
 
-            Vector3 modelEuler = characterModel.transform.rotation.eulerAngles;
-            Quaternion modelTargetRotation = Quaternion.Euler(0f, modelEuler.y, 0f);
+            //Vector3 modelEuler = characterModel.transform.rotation.eulerAngles;
+            //Quaternion modelTargetRotation = Quaternion.Euler(0f, modelEuler.y, 0f);
             //characterModel.transform.rotation = Quaternion.Slerp(characterModel.transform.rotation, modelTargetRotation, groundRotationRate * Time.fixedDeltaTime);
 
-            Vector3 bodyEuler = transform.rotation.eulerAngles;
-            Quaternion bodyTargetRotation = Quaternion.Euler(0f, bodyEuler.y, 0f);
+            //Vector3 bodyEuler = transform.rotation.eulerAngles;
+            //Quaternion bodyTargetRotation = Quaternion.Euler(0f, bodyEuler.y, 0f);
             //transform.rotation = Quaternion.Slerp(transform.rotation, bodyTargetRotation, groundRotationRate * Time.fixedDeltaTime);
         }
 
