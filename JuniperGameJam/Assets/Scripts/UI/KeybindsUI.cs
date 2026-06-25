0b27a790777d45739ec0c61cbd42aca17a0027aa
”TimCrean” <tim.c.crean@gmail.com>
2026-06-25 10:03:36 -0400

    Keybinds Fixes

--- JuniperGameJam/Assets/Scripts/UI/KeybindsUI.cs
+++ JuniperGameJam/Assets/Scripts/UI/KeybindsUI.cs
@@ -4,6 +4,16 @@ using UnityEngine.UI;
 public class KeybindsUI : MonoBehaviour
 {
     [SerializeField] private OptionsUI OptionsCanvas;
+    [SerializeField] private Button backButton;
+    [SerializeField] private Button defaultButton;
+    private ChangeBindingsScript changeBindingsScript;
+
+    void Start()
+    {
+        changeBindingsScript = GetComponent<ChangeBindingsScript>();
+        defaultButton.onClick.AddListener(changeBindingsScript.ResetBindings);
+        backButton.onClick.AddListener(Hide);
+    }
     
     public void Show()
     {
